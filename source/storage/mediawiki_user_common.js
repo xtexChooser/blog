b18f7775adccc07b06be3a02dde0c045ab666a70
xtex <zbwxtex@qq.com>
2022-01-01 10:45:06 +0800

    feat: add some user script for fandom

--- source/storage/mediawiki_user_common.js
+++ source/storage/mediawiki_user_common.js
@@ -1,2 +1,18 @@
 mw.loader.load('https://wikiplus-app.com/Main.js');
 mw.loader.load('https://backup.blog.xtexx.ml/storage/mediawiki_auto_minor.js');
+
+// Fandom
+if(location.hostname.endsWith("fandom.com")) {
+    importArticles({
+        type: 'script',
+        articles: [
+            'u:dev:MediaWiki:NoImageLightbox/code.js',
+            'u:dev:MediaWiki:View_Source/code.js',
+            'u:dev:MediaWiki:LinkPreview/code.js',
+            'u:dev:MediaWiki:MassEdit/code.js',
+            'u:dev:MediaWiki:View_Raw/code.js',
+            'u:dev:MediaWiki:UserUnusedFiles/code.js',
+            'u:dev:MediaWiki:HTML5AudioPlayer/code.js',
+        ]
+    });
+}
